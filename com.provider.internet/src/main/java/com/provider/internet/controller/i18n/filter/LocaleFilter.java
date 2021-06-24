22ca61442f6ab21929d9423ef430f05618ea9cfe
Shabanoff <shabanoff025@gmail.com>
2021-06-25 01:38:18 +0300

    security

--- com.provider.internet/src/main/java/com/provider/internet/controller/i18n/filter/LocaleFilter.java
+++ com.provider.internet/src/main/java/com/provider/internet/controller/i18n/filter/LocaleFilter.java
@@ -5,7 +5,9 @@ import com.provider.internet.controller.i18n.SupportedLocale;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Locale;
@@ -25,13 +27,13 @@ public class LocaleFilter implements Filter {
                          FilterChain chain)
             throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
-
+        HttpServletResponse resp = (HttpServletResponse) response;
         if(req.getParameter(LANG) != null) {
-            replaceUserLocale(req);
+            replaceUserLocale(req, resp);
         }
 
-        if (req.getSession().getAttribute(LOCALE) == null) {
-            setUserLocale(req);
+        if (req.getSession().getAttribute(LOCALE) == null && req.getCookies().length!=0) {
+            setUserLocale(req, resp);
         }
 
         chain.doFilter(request, response);
@@ -42,14 +44,18 @@ public class LocaleFilter implements Filter {
 
     }
 
-    private void replaceUserLocale(HttpServletRequest request) {
+    private void replaceUserLocale(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession();
         String langParameter = request.getParameter(LANG);
+        Cookie localeCookie_lang = new Cookie( "locale", SupportedLocale.getLocaleOrDefault(langParameter).getLanguage() );
+        response.addCookie( localeCookie_lang );
         Locale locale = SupportedLocale.getLocaleOrDefault(langParameter);
         session.setAttribute(LOCALE, locale);
     }
 
-    private void setUserLocale(HttpServletRequest request) {
+    private void setUserLocale(HttpServletRequest request, HttpServletResponse response) {
+        Cookie localeCookie_lang = new Cookie( "locale", SupportedLocale.getDefault().getLanguage() );
+        response.addCookie( localeCookie_lang );
         HttpSession session = request.getSession();
         Locale locale = SupportedLocale.getDefault();
         session.setAttribute(LOCALE, locale);
